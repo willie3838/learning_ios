1b62e14d284f5681c21a82a9516c229589f63aae
William Wai Lim Chan <wchan365@uwo.ca>
2022-06-17 21:01:49 -0400

    finished split bill project

--- split_bill/split_bill/ContentView.swift
+++ split_bill/split_bill/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var totalCost = ""
+    @State private var people = 1
+    @State private var tipIndex = 0
+    let tipPercentages = [0,5,10,15]
+    
+    func calculateTotal() -> Double {
+        let tip = Double(tipPercentages[tipIndex])
+        let orderTotal = Double(totalCost) ?? 0
+        
+        let finalAmount = ((orderTotal / 100 * tip) + orderTotal)
+        return finalAmount / Double(people)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Enter an amount")) {
+                    TextField("Amount", text: $totalCost).keyboardType(.decimalPad)
+                }
+                Section(header: Text("Select a tip amount (%)")) {
+                    Picker("Tip percentage", selection: $tipIndex){
+                        ForEach(0 ..< tipPercentages.count, id: \.self) {
+                            Text("\(tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("How many people?")) {
+                    Picker("Number of people", selection: $people){
+                        ForEach(0..<25){
+                            Text("\($0) people")
+                        }
+                    }
+                    
+                }
+                Section(header: Text("Total per person")) {
+                    Text("$\(calculateTotal(), specifier: "%.2f")")
+                }
+            }.navigationTitle("Split the Bill")
+        }
     }
 }
 
