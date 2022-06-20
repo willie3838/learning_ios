3815c6dc4a3168e155814817980767c9c5caeba6
William Wai Lim Chan <wchan365@uwo.ca>
2022-06-19 21:57:12 -0400

    finished flag project

---
+++ guess_the_flag/guess_the_flag/guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guess_the_flagApp.swift
+//  guess_the_flag
+//
+//  Created by William Chan on 2022-06-19.
+//
+
+import SwiftUI
+
+@main
+struct guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
