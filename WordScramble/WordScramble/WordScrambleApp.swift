4951fad1a71595f242f8e82923073592b1c446ca
William Wai Lim Chan <wchan365@uwo.ca>
2022-06-21 15:53:28 -0400

    finished word scramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by William Chan on 2022-06-21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
